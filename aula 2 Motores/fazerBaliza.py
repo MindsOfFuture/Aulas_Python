5fa8f348cc0161271d4020bafe46d16698538e5e
Rafael Rocha <49845067+Rafaelrr5@users.noreply.github.com>
2024-09-16 13:34:07 -0300

    Update fazerBaliza.py (agora certin)

--- aula 2 Motores/fazerBaliza.py
+++ aula 2 Motores/fazerBaliza.py
@@ -1,18 +1,20 @@
 from hub import port as porta
 import runloop as Lego
 import motor
-import motor_pair as par_motor
+import motor_pair
 #fazer baliza
-async def main():
-    motor_pair.pair(par_motor.PAIR_1, porta.B, porta.A)
+async def main(): 
+    motor_pair.pair(motor_pair.PAIR_1, porta.B, porta.A)
 
-    await motor_pair.move_tank_for_time(par_motor.PAIR_1, 1000,2000,2000)
-    await motor.run_for_degrees(porta.A, degrees=277, velocity=720)
-    await motor.run_for_degrees(porta.A, degrees=277, velocity=720)
-    await motor.pair.move_tank_for_time(par_motor.PAIR_1, 1000,2000,2000)
+    await motor_pair.move_tank_for_time(motor_pair.PAIR_1, 1000, 2000, 2000)
+    await motor.run_for_degrees(porta.A, 277, 720)
+    await motor.run_for_degrees(porta.A, 277, 720)
+    await motor_pair.move_tank_for_time(motor_pair.PAIR_1, 1000,2000,2000)
 
     #Anda fazendo curva
-    await motor_pair.move_for_time(par_motor.PAIR_1, 20000,20,velocity=400)
+    await motor_pair.move_for_time(motor_pair.PAIR_1, 2000,20, velocity=400)
 
     #Anda reto
-    await motor_pair.move_for_time(par_motor.PAIR_1, 1000,-20,velocity=-400)
+    await motor_pair.move_for_time(motor_pair.PAIR_1, 1000,-20, velocity=-400)
+
+Lego.run(main())
